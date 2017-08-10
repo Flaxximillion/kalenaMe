5482358e6e0b6ea8206b4e35938025c302bc735b
AaronKWalker <aaron.walker@outlook.com>
2017-08-10 13:11:50 -0500

    completed new models

--- models/message.js
+++ models/message.js
@@ -1,28 +1,35 @@
 module.exports = function (sequelize, DataTypes) {
     var Message = sequelize.define("Message", {
-        messageText: {
-            type: DataTypes.TEXT,
-            validate: {
-                notNull: true,
-                notEmpty: true
-            }
-        },
-        messageSender: {
-            type: DataTypes.STRING
-        },
-        messageReceiver: {
-            type: DataTypes.STRING
+      messageID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        validate: {
+          notNull: true,
+          notEmpty: true
         }
-    });
+      },
+      messageCalendar: {
+        type: DataTypes.STRING,
+        validate: {
+          notNull: true,
+          notEmpty: true
+        }
+      },
+      messageSender: {
+        type: DataTypes.STRING
+      },
+      messageReceiver: {
+        type: DataTypes.STRING
+      },
+      messageBody: {
+        type: DataTypes.TEXT,
+        validate: {
+            notNull: true,
+            notEmpty: true
+        }
+      }
+  });
 
-    //a message can't be created without a task
-    Message.associate = function (models) {
-        Message.belongsTo(models.Group, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
-
-    return Message;
+  return Message;
 };
--- models/task.js
+++ models/task.js
@@ -1,49 +1,67 @@
 module.exports = function (sequelize, DataTypes) {
     var Task = sequelize.define("Task", {
-        taskName: {
-            type: DataTypes.STRING,
-            validate: {
-                isAlphanumeric: true,
-                notNull: true,
-                notEmpty: true,
-                len: [5, 100]
-            }
-        },
-        taskDescription: DataTypes.TEXT,
-        taskAccepted: {
-            type: DataTypes.BOOLEAN,
-            default: false,
-            validate: {
-                notNull: true
-            }
-        },
-        taskDateNeeded: {
-            type: DataTypes.DATEONLY,
-            validate: {
-                notEmpty: true,
-                notNull: true,
-                isDate: true
-            }
-        },
-        taskTime: {
-            type: DataTypes.TIME,
-            validate: {
-                notEmpty: true,
-                notNull: true
-            }
+      taskID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        validate: {
+          notNull: true,
+          notEmpty: true
         }
+      },
+      taskCalendar: {
+        type: DataTypes.INTEGER,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+          isNumeric: true
+        }
+      }
+      taskName: {
+        type: DataTypes.STRING,
+        validate: {
+          isAlphanumeric: true,
+          notNull: true,
+          notEmpty: true,
+          len: [5, 100]
+        }
+      },
+      taskDescription: DataTypes.TEXT,
+      taskAccepted: {
+        type: DataTypes.BOOLEAN,
+        default: false,
+        validate: {
+          notNull: true
+        }
+      },
+      taskRequester: {
+        type: DataTypes.STRING,
+        validate: {
+          notNull: true,
+          notEmpty: true
+        }
+      },
+      taskAccepter: {
+        type: DataTypes.STRING,
+      },
+      taskDate: {
+        type: DataTypes.DATEONLY,
+        validate: {
+          notEmpty: true,
+          notNull: true,
+          isDate: true
+        }
+      },
+      taskTime: {
+        type: DataTypes.TIME,
+        validate: {
+          notEmpty: true,
+          notNull: true
+        }
+      }
     }, {
-        timestamps: false
+      timestamps: false
     });
 
-    Task.associate = function (models) {
-        Task.belongsTo(models.Calendar, {
-            onDelete: "CASCADE",
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
-
-    return Task;
+  return Task;
 };
