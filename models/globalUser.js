fec165053874fc0774bc2066e927e5a390f049f7
AaronKWalker <aaron.walker@outlook.com>
2017-08-11 19:05:39 -0500

    routing issues

--- models/globalUser.js
+++ models/globalUser.js
@@ -2,53 +2,51 @@ module.exports = function (sequelize, DataTypes) {
     var GlobalUser = sequelize.define("globalUser", {
       globalUserUUID: {
         type: DataTypes.STRING,
-        primaryKey: true,
-        validate: {
-          notNull: true,
-          notEmpty: true
-        }
+        primaryKey: true
+        // validate: {
+        //   //allowNull: false,
+        //   notEmpty: true
+        // }
       },
       globalUserFirstName: {
-          type: DataTypes.STRING,
-          unique: true,
-          validate: {
-              isAlphanumeric: true,
-              notNull: true,
-              notEmpty: true
-          }
+          type: DataTypes.STRING
+          // validate: {
+          //     isAlphanumeric: true,
+          //     //allowNull: false,
+          //     notEmpty: true
+          // }
       },
       globalUserLastName: {
-          type: DataTypes.STRING,
-          unique: true,
-          validate: {
-              isAlphanumeric: true,
-              notNull: true,
-              notEmpty: true
-          }
+          type: DataTypes.STRING
+          // validate: {
+          //     isAlphanumeric: true,
+          //     //allowNull: false,
+          //     notEmpty: true
+          // }
       },
       globalUserPhone: {
-          type: DataTypes.STRING,
-          validate: {
-              not: ["[a-z]", 'i'],
-              notNull: true,
-              notEmpty: true
-          }
+          type: DataTypes.STRING
+          // validate: {
+          //     not: ["[a-z]", 'i'],
+          //     //allowNull: false,
+          //     notEmpty: true
+          // }
       },
       globalUserEmail: {
-          type: DataTypes.STRING,
-          validate: {
-              isEmail: true,
-              notNull: true,
-              notEmpty: true
-          }
+          type: DataTypes.STRING
+          // validate: {
+          //     isEmail: true,
+          //     //allowNull: false,
+          //     notEmpty: true
+          // }
       },
       globalUserPassword: {
-          type: DataTypes.STRING,
-          validate: {
-              notNull: true,
-              notEmpty: true,
-              len: [5]
-          }
+          type: DataTypes.STRING
+          // validate: {
+          //     //allowNull: false,
+          //     notEmpty: true,
+          //     len: [5]
+          // }
       }
     }, {
         timestamps: false
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var models = require("../models");
+var bodyParser = require('body-parser');
 
-router.get('/:id', function(req, res, next){
-    res.send('test');
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// router.get('/:id', function(req, res, next){
+//     res.send('test');
+// });
+
+//get all users from db
+router.get('/all', function(req, res, next){
+    models.globalUser.findAll({}).then(function(dbUser){
+      res.json(dbUser);
+    });
 });
 
-router.post('/submit/', function(req, res, next){
-    res.send('test');
+//creates new user
+router.post('/submit/',urlencodedParser, function(req, res, next){
+  console.log(req.body);
+    models.globalUser.create(req.body).then(function(dbUser){
+      res.send("new user added to database");
+    }).catch(function(err){
+      console.log("");
+      console.log("~~~ERROR~~~ERROR~~~ERROR~~~ERROR~~~ERROR~~~ERROR~~~ERROR~~~ERROR~~~");
+      console.log(err.errors);
+    });
 });
 
 module.exports = router;
