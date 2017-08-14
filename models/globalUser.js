eb4af7c819b47428d4ec967af9b2a989d0c0c09e
Flaxximillion <ayu2013@gmail.com>
2017-08-14 08:39:08 -0500

    Base hash and passport

--- models/globalUser.js
+++ models/globalUser.js
@@ -1,51 +1,52 @@
 module.exports = function (sequelize, DataTypes) {
-    var GlobalUser = sequelize.define("GlobalUser", {
-      globalUserUUID: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        primaryKey: true,
-        validate: {
-          notEmpty: true
+    var GlobalUser = sequelize.define("globalUser", {
+            globalUserUUID: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                primaryKey: true,
+                validate: {
+                    notEmpty: true
+                }
+            },
+            globalUserFirstName: {
+                type: DataTypes.STRING,
+                validate: {
+                    isAlphanumeric: true,
+                    notEmpty: true
+                }
+            },
+            globalUserLastName: {
+                type: DataTypes.STRING,
+                validate: {
+                    isAlphanumeric: true,
+                    notEmpty: true
+                }
+            },
+            globalUserPhone: {
+                type: DataTypes.STRING,
+                validate: {
+                    not: ["[a-z]", 'i'],
+                    notEmpty: true
+                }
+            },
+            globalUserEmail: {
+                type: DataTypes.STRING,
+                validate: {
+                    isEmail: true,
+                    notEmpty: true
+                }
+            },
+            globalUserHash: {
+                type: DataTypes.STRING,
+                validate: {
+                    notEmpty: true,
+                    len: [5]
+                }
+            }
         }
-      },
-      globalUserFirstName: {
-          type: DataTypes.STRING,
-          validate: {
-              isAlphanumeric: true,
-              notEmpty: true
-          }
-      },
-      globalUserLastName: {
-          type: DataTypes.STRING,
-          validate: {
-              isAlphanumeric: true,
-              notEmpty: true
-          }
-      },
-      globalUserPhone: {
-          type: DataTypes.STRING,
-          validate: {
-              not: ["[a-z]", 'i'],
-              notEmpty: true
-          }
-      },
-      globalUserEmail: {
-          type: DataTypes.STRING,
-          validate: {
-              isEmail: true,
-              notEmpty: true
-          }
-      },
-      globalUserPassword: {
-          type: DataTypes.STRING,
-          validate: {
-              notEmpty: true,
-              len: [5]
-          }
-      }
-    }, {
-        timestamps: false
-    });
+        , {
+            timestamps: false
+        });
 
-  return GlobalUser;
+    return GlobalUser;
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,61 +2,85 @@ var express = require('express');
 var router = express.Router();
 var models = require("../models");
 var bodyParser = require('body-parser');
+var uuid = require('uuid/v4');
+var bcrypt = require('bcryptjs');
 
 var jsonParser = bodyParser.json();
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-router.get('/api/:id', function(req, res, next){
+router.get('/api/:id', function (req, res, next) {
     if (req.params.id === "all") {
-      //return all users
-      models.globalUser.findAll({})
-      .then(function(dbUser){
-        res.json(dbUser);
-      }).catch(function(err){
-        catchErr(err);
-      });
+        //return all users
+        models.globalUser.findAll({})
+            .then(function (dbUser) {
+                res.json(dbUser);
+            }).catch(function (err) {
+            catchErr(err);
+        });
     } else {
-      //return specific user
-      models.globalUser.findOne({
-        where: {
-          globalUserFirstName: req.params.firstName,
-          globalUserLastName: req.params.lastName,
-          globalUserPassword: req.params.password
-        }
-      }).then(function(dbUser){
-        res.json(dbUser)
-      }).catch(function(){
-        catchErr(err);
-      });
+        //return specific user
+        models.globalUser.findOne({
+            where: {
+                globalUserFirstName: req.params.firstName,
+                globalUserLastName: req.params.lastName,
+                globalUserPassword: req.params.password
+            }
+        }).then(function (dbUser) {
+            res.json(dbUser)
+        }).catch(function () {
+            catchErr(err);
+        });
     }
 });
 
-//get all users from db
-// router.get('/all', function(req, res, next){
-//     models.globalUser.findAll({}).then(function(dbUser){
-//       res.json(dbUser);
-//     });
-// });
+router.post('/login/', urlencodedParser, function(req, res, next){
+    models.globalUser.findOne({
+        where:{
+            globalUserEmail: req.body.email
+        },
+        raw: true
+    }).then(function(user){
+        console.log(user);
+        bcrypt.compare(req.body.password, user.globalUserHash).then(function(result){
+            console.log(result);
+        })
+    })
+});
 
 
 //creates new user
-router.post('/submit/',urlencodedParser, function(req, res, next){
-  console.log(req.body);
-    models.globalUser.create(req.body)
-    .then(function(dbUser){
-      res.send("new user added to database");
-      //redirect to calendar list/create page
-    }).catch(function(err){
-      catchErr(err);
+router.post('/submit/', urlencodedParser, function (req, res, next) {
+    var userData = {
+        globalUserUUID: uuid(),
+        globalUserFirstName: req.body.firstName,
+        globalUserLastName: req.body.lastName,
+        globalUserPhone: req.body.phone,
+        globalUserEmail: req.body.email
+    };
+
+    bcrypt.genSalt(10, function(err, salt){
+        console.log(salt);
+        bcrypt.hash(req.body.password, salt, function(err, hash){
+            console.log(hash);
+            userData.globalUserHash = hash;
+
+            models.globalUser.create(userData)
+                .then(function (dbUser) {
+                    res.send("new user added to database");
+                    //redirect to calendar list/create page
+                }).catch(function (err) {
+                catchErr(err);
+            });
+        })
     });
 });
 
 
 //displays error if one occurs
-function catchErr(err){
-  console.log("");
-  console.log("~~~ERROR~~~ERROR~~~ERROR~~~ERROR~~~ERROR~~~ERROR~~~ERROR~~~ERROR~~~");
-  console.log(err.errors);
+function catchErr(err) {
+    console.log("");
+    console.log("~~~ERROR~~~ERROR~~~ERROR~~~ERROR~~~ERROR~~~ERROR~~~ERROR~~~ERROR~~~");
+    console.log(err);
 }
 
 module.exports = router;
